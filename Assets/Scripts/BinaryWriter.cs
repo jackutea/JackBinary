44439c9e274db2e1a0edd18fe502bba3b163fa84
Jackutea <chenwansal1@163.com>
2024-01-31 20:08:36 +0800

    ESTABLISH

---
+++ Assets/Scripts/BinaryWriter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JackBinary {
+
+    public static class BinaryWriter  {
+
+        public static void Yo() {
+            Debug.Log("Yo");
+        }
+
+    }
+
+}
